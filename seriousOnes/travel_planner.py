b2bfcb70814c9cd693e947d3a0e05085d44bb624
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-07-29 19:35:03 +0500

    Add travel planner project

---
+++ seriousOnes/travel_planner.py
@@ -0,0 +1,51 @@
+#travel planner
+#difficulty  *
+
+destination = input("Destination city: ")
+num_night = int(input('Number of nights: '))
+hotel_price = int(input("Hotel price per night: "))
+food_budget = int(input("Food budget per day: "))
+transport_cost = int(input("Transportation cost: "))
+other_ex = int(input("Other expenses: "))
+
+hotel_cost = hotel_price * num_night
+food_cost = food_budget * num_night
+
+total = num_night * (hotel_price + food_budget) + transport_cost + other_ex
+avg_per_day = total / num_night
+
+print()
+
+print("--------- TRAVEL PLAN ---------")
+print()
+
+print(f"Destination: {destination}")
+print()
+
+print(f"Days: {num_night} ")
+print()
+
+print(f"Hotel cost: {hotel_cost} uzs")
+print()
+
+print(f"Food cost: {food_cost} uzs")
+print()
+
+print(f"Transportation: {transport_cost} uzs")
+print()
+
+print(f"Other expences: {other_ex} uzs")
+print()
+
+print("----------------------------------------")
+print()
+
+print("Total Trip Costs: ")
+print(total)
+print()
+
+print("Average Per Day: ")
+print(avg_per_day)
+print()
+
+print("===========================================")
