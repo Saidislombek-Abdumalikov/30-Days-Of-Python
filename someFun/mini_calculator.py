40cbc1a92fcebfd4811d20d55c81c10cd8580e0a
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-07-29 09:21:28 +0500

    Added Mini Calculaotor

---
+++ someFun/mini_calculator.py
@@ -0,0 +1,9 @@
+#mini calculator
+
+first_num = int(input("First number: "))
+second_num = int(input("Second number: "))
+
+print("Addition: ", first_num + second_num)
+print("Subtraction: ", first_num - second_num)
+print("Multiplication: ", first_num * second_num)
+print("Division: ", int( first_num / second_num))
