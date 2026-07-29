7215b898093121ab41d87efbcddfeb6546a00d19
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-07-29 22:10:19 +0500

    Code updated to V1.1

--- seriousOnes/fuel_cost_calculator.py
+++ seriousOnes/fuel_cost_calculator.py
@@ -7,15 +7,17 @@ trip_distance = int(input("Trip distance (km): "))
 avg_speed = int(input("Average driving speed (km/h): "))
 fuel_consumption = int(input("Fuel consumption (litres per 100 km): "))
 fuel_price = float(input("Fuel price (per liter): "))
+budget= float(input("Budget (uzs): "))
 #input
 
 fuel_needed = trip_distance * fuel_consumption / 100          #calculates the liter of fuel needed for trip
-total_trip_cost = fuel_needed * fuel_price
-time_to_reach_h = trip_distance / avg_speed
-time_to_reach_M = int(time_to_reach_h * 60)
+total_trip_cost = fuel_needed * fuel_price                    #calcualtes the total cost  
+time_to_reach_h = trip_distance / avg_speed                   #calculates the time in hours
+time_to_reach_M = int(time_to_reach_h * 60)                   #calcualtes the time in minutes
+remaining_money = budget - total_trip_cost                    #calculates the money left
 
 print()
-print("--------- Fuel --------")
+print("--------- FUEL TRIP REPORT --------")
 print() 
 
 print(f"Car Model: {car_name}")
@@ -34,6 +36,9 @@ print(f"Fuel needed: {fuel_needed}")
 print()
 
 print(f"Total trip cost: {total_trip_cost} uzs")
+print()
+
+print(f"Remaining money: {remaining_money} uzs")
 print()
 
 print("============================")
