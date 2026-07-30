85a60f0810af18c99b95bd22fd01737e69d5bd5d
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-07-30 12:27:04 +0500

    Updated to v1.1

--- seriousOnes/electricity_bill_estitmator.py
+++ seriousOnes/electricity_bill_estitmator.py
@@ -1,7 +1,7 @@
 #electricity bill estimator
 
 customer_name = input("Customer name: ")
-pre_meter = int(input("Prvious meter: "))
+pre_meter = int(input("Previous meter: "))
 curr_meter = int(input("Current meter: "))
 price = int(input("Price per kWh: "))
 budget = int(input("Monthly budget: "))
@@ -9,6 +9,10 @@ budget = int(input("Monthly budget: "))
 e_used = curr_meter - pre_meter
 total_bill = e_used * price
 remaining_budget = budget - total_bill
+avg_e_used_day = e_used / 30
+avg_m_used_day = total_bill / 30
+percentage_budget_spent = total_bill * 100 / budget
+percentage_budget_remaining = remaining_budget * 100 / budget
 
 print()
 print("---------- ELECTRICITY BILL REPORT ----------")
@@ -26,7 +30,13 @@ print()
 print(f"Electricity used: {e_used} kWh")
 print()
 
+print(f"Average electricity used per day: {avg_e_used_day} kWh")
+print()
+
 print(f"Price per kWh: {price}")
+print()
+
+print(f"Average money spent per day: {avg_m_used_day} uzs")
 print()
 
 print(f"Total Bill: {total_bill} uzs")
@@ -34,5 +44,12 @@ print()
 
 print(f"Remaining budget: {remaining_budget} uzs")
 print()
+
+print(f"Percentage of the budget spent : {percentage_budget_spent} %")
+print()
+
+print(f"Percentage of the budget remaining : {percentage_budget_remaining} %")
+print()
+
 
 print("=============================================")
