65668f52a9f755466ffb2b8d04b2d39362c2082e
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-07-31 11:25:50 +0500

    Built Salary Analyzer

--- seriousOnes/salary_analyzer.py
+++ seriousOnes/salary_analyzer.py
@@ -1,61 +1,10 @@
-#salary analyzer 
-
-'''name = input("Employee name:")
-employee_id = input("Employee ID:")
-position = input("Position: ")
-shift = input("Shift (day/night): ")
-working_hours = int(input("Working hours per month: "))
-salary_month = input("Salary month: (eg.,June): ")
-base_salary = int(input("Base salary: "))
-bonus_salary = int(input("Bonus salary: "))
-rental = int(input("Rental cost: "))
-food = int(input("Food cost: "))
-transport = int(input("Transportation cost: "))
-rent = int(input("Rented money: "))
-lending = int(input("Money lent to others: "))
-debt = int(input("Debt: "))
-
-heading_info= f"\n----------- EMPLOYEE INFO -------------\n"
-heading_income = f"\n----------- INCOME -------------\n"
-heading_expense = f"\n----------- EXPENSE -------------\n"
-heading_summary = f"\n----------- FINANCIAL SITUATION -------------\n"
-
-total_income = base_salary + bonus_salary
-total_expense = food + rent + transport +rental
-
-money_saved = total_income - total_expense
-
-income_per_hour = total_income / working_hours
-
-
-
-
-print(heading_info)
-print(f"\nName: {name}\n")
-print(f"ID: {employee_id}\n")
-print(f"Position: {position}\n")
-print(f"Shift: {shift}\n")
-print(f"Salary month: {salary_month}\n")
-
-print(heading_income)
-print(f"Monthly Salary: {base_salary} uzs\nBonus: {bonus_salary} uzs\nTotal Income: {total_income} uzs\nIncome per hour: {income_per_hour:.1f}")
-
-print(heading_expense)
-print(f"Rented money: {rent} uzs\nRental cost: {rental} uzs\nFood: {food} uzs\nTransportation: {transport} uzs\nTotal Expense: {total_expense} uzs\n")
-
-print(heading_summary)
-
-print(f"Money saved: {money_saved} uzs\nDebt: {debt}\nMoney lent to others: {lending}")
-
-print("===========================================================")'''
-
 '''INPUT'''
 employee_name = input("Employee name:")
 employee_id = input("Employee ID: ")
 position = input("Employee postion: ")
 #work
 working_schedule = input("Working schedule (day/night): ")
-working_hours = int(input("Working hours: "))
+monthly_working_hours = int(input("Working hours: "))
 #month
 month_name = input("Month name (eg.,June): ")
 #salary
@@ -77,17 +26,17 @@ heading_expense = "\n-------- EXPENSE INFORMATION -------\n"
 heading_summary = "\n------------ SUMMARY ------------\n"
 
 '''FORMULA'''
-total_income = base_salary + bonus_salary + debt
-total_expense = rent + food + transport + other + lent
+total_income = base_salary + bonus_salary
+total_expense = rent + food + transport + other
 saved_money = total_income - total_expense
 #income-per
 income_per_week = total_income / 4 
 income_per_day = total_income / 30
-income_per_hour = total_income / working_hours
+income_per_hour = total_income / monthly_working_hours
 #expensee-per
 expense_per_week = total_expense / 4
 expense_per_day = total_expense / 30
-expense_per_hour = total_expense / working_hours
+expense_per_hour = total_expense / monthly_working_hours
 
 '''PRINT'''
 print(heading_info)
@@ -96,37 +45,37 @@ print(f"\nEmployee Name: {employee_name}\n")
 print(f"Employee ID: {employee_id}\n")
 print(f"Current Position: {position}\n")
 print(f"Working Schedule: {working_schedule}\n")
-print(f"Working Hours: {working_hours}\n")
+print(f"Working Hours: {monthly_working_hours}\n")
 
 print(heading_income)
 
 print(f"Month Name: {month_name}\n")
-print(f"Base Salary: {base_salary} uzs\n")
-print(f"Bonus Salary: {bonus_salary} uzs\n")
+print(f"Base Salary: {base_salary} usd\n")
+print(f"Bonus Salary: {bonus_salary} usd\n")
 
 
 print(heading_expense)
 
-print(f"Rent: {rent} uzs\n")
-print(f"Food: {food} uzs\n")
-print(f"Transport: {transport} uzs\n")
-print(f"Others: {other} uzs\n")
-print(f"Lent: {lent} uzs\n")
-print(f"Debt: {debt} uzs\n")
+print(f"Rent: {rent} usd\n")
+print(f"Food: {food} usd\n")
+print(f"Transport: {transport} usd\n")
+print(f"Others: {other} usd\n")
+print(f"Lent: {lent} usd\n")
+print(f"Debt: {debt} usd\n")
 
 
 print(heading_summary)
 
-print(f"Total Income: {total_income} uzs\n")
+print(f"Total Income: {total_income} usd\n")
 print(f"Income Per:\nWeek   \tDay   \tHour")
-print(f"Income per Week: {income_per_week:.1f} uzs\n")
-print(f"Income per Day {income_per_day:.1f} uzs\n")
-print(f"Income per Hour {income_per_hour:.1f} uzs\n\n")
+print(f"Income per Week: {income_per_week:.1f} usd\n")
+print(f"Income per Day {income_per_day:.1f} usd\n")
+print(f"Income per Hour {income_per_hour:.1f} usd\n\n")
 
-print(f"Total Expense: {total_expense} uzs\n")
+print(f"Total Expense: {total_expense} usd\n")
 print(f"Expense Per:\nWeek   \tDay   \tHour")
-print(f"Expense per Week: {expense_per_week:.1f} uzs\n")
-print(f"Expense per Day {expense_per_day:.1f} uzs\n")
-print(f"Expense per Hour {expense_per_hour:.1f} uzs\n")
+print(f"Expense per Week: {expense_per_week:.1f} usd\n")
+print(f"Expense per Day {expense_per_day:.1f} usd\n")
+print(f"Expense per Hour {expense_per_hour:.1f} usd\n")
 
 print(f"Saved Money: {saved_money}")
