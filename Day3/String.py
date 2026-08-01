42e79074d90f8684ef06d0f8ab007470ad51e8ad
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-08-01 22:31:21 +0500

    String Concatenation

--- Day3/String.py
+++ Day3/String.py
@@ -26,3 +26,15 @@ print(multiline_string)
 
 
 '''//////////////////////////////////////////////////////////////'''
+
+
+first_name = 'Saidislom'
+last_name = 'Abdumalikov'
+space = ' '
+full_name = first_name  +  space + last_name
+print(full_name) # Saidislom Abdumalikov
+# Checking the length of a string using len() built-in function
+print(len(first_name))  # 8
+print(len(last_name))   # 7
+print(len(first_name) > len(last_name)) # True
+print(len(full_name)) # 16
