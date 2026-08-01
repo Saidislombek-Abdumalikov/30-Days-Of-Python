b4eb95fdc2feaf4dfc735b2ff36972f1700db97a
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-08-01 22:25:21 +0500

    Multi line

--- Day3/String.py
+++ Day3/String.py
@@ -4,5 +4,25 @@ print(len(letter))          # 1
 greeting = 'Hello, World!'  # String could be made using a single or double quote,"Hello, World!"
 print(greeting)             # Hello, World!
 print(len(greeting))        # 13
-sentence = "I hope you are enjoying 30 days of Python Challenge"
+sentence = "I am enjoying 30 days of Python Challenge"
 print(sentence)
+
+
+
+'''///////////////////////////////////////////////////////'''
+
+
+
+multiline_string = '''I am a student and enjoy learning.
+I didn't find anything as rewarding as learning to code.
+That is why I created 30 days of python.'''
+print(multiline_string)
+
+# Another way of doing the same thing
+multiline_string = """I am a student and enjoy learning.
+I didn't find anything as rewarding as learning to code.
+That is why I created 30 days of python.'''"""
+print(multiline_string)
+
+
+'''//////////////////////////////////////////////////////////////'''
