c145db6a02bb662ea4b7813918e80ec321c3579e
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-08-01 22:34:20 +0500

    Escape sequences

--- Day3/String.py
+++ Day3/String.py
@@ -38,3 +38,30 @@ print(len(first_name))  # 8
 print(len(last_name))   # 7
 print(len(first_name) > len(last_name)) # True
 print(len(full_name)) # 16
+
+
+'''////////////////////////////////////////////////////////////////'''
+
+
+print('I am enjoying the Python Challenge.\nI am ?') # line break
+print('Days\tTopics\tExercises') # adding tab space or 4 spaces
+print('Day 1\t5\t5')
+print('Day 2\t6\t20')
+print('Day 3\t5\t23')
+print('Day 4\t1\t35')
+print('This is a backslash  symbol (\\)') # To write a backslash
+print('In every programming language it starts with \"Hello, World!\"') # to write a double quote inside a single quote
+
+# output
+'''I am enjoying the Python Challenge.
+I am ?
+Days  Topics  Exercises
+Day 1	5	    5
+Day 2	6	    20
+Day 3	5	    23
+Day 4	1	    35
+This is a backslash  symbol (\)
+In every programming language it starts with "Hello, World!"'''
+
+
+'''////////////////////////////////////////////////////////////////////// '''
