677c65c3a31a0922899778ccde1848129d9cde5f
Saidislom <saidislomabdumalikov1234@gmail.com>
2026-08-02 16:44:17 +0500

    v1.0 code updated

--- seriousOnes/freelancer_invoice_generator.py
+++ seriousOnes/freelancer_invoice_generator.py
@@ -2,29 +2,37 @@
 
 freelancer_name = input("Freelancer name: ")
 client_name = input("Client name: ")
-client_tel = int(input("+998 "))
+client_tel = input("+998 ")
 invoice_id = input("Invoice ID: ")
 invoice_month = input("Invoice month (eg.June): ")
 project_name = input("Project name:")
-hours_worked = int(input("Hours woked: "))
+hours_worked = int(input("Hours worked: "))
 hourly_rate = int(input("Hourly rate (USD): "))
 
+'''FORMULA'''
 total_payment = hourly_rate * hours_worked
 
-print("\nFREELANCER".center(50))
+'''HEADING'''
+heading_freelancer = "FREELANCER"
+heading_client = "CLIENT"
+heading_invoice = "INVOICE"
 
-print(f"\nFreelancer Name: {freelancer_name}\n")
 
-print("\nCLIENT".center(50))
+'''PRINT'''
+print("\n",heading_freelancer.center(50))
 
-print(f"\nClient Name: {client_name}")
-print(f"Client Phone Number: {client_tel}\n")
+print(f"\nFreelancer Name: {freelancer_name.title()}\n")
 
-print("\nINVOICE".center(50))
+print("\n",heading_client.center(50))
 
-print(f"\nInvoice ID: {invoice_id}")
-print(f"Month: {invoice_month}")
-print(f"Project name: {project_name}")
+print(f"\nClient Name: {client_name.title()}")
+print(f"Client Phone Number: +998 {client_tel}\n")
+
+print("\n",heading_invoice.center(50))
+
+print(f"\nInvoice ID: {invoice_id.upper()}")
+print(f"Month: {invoice_month.upper()}")
+print(f"Project name: {project_name.title()}")
 print(f"Hours worked: {hours_worked}")
 print(f"Hourly rate: {hourly_rate}")
 print(f"Total payment: {total_payment}")
